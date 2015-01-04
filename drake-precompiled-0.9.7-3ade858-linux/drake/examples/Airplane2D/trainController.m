08cdf9609fb6b0386c53d41d64f0e6dc3c855697
Beomjoon Kim <beomjoon.kim0@gmail.com>
2015-01-04 15:32:41 -0500

    asdf

--- drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainController.m
+++ drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainController.m
@@ -1,11 +1,11 @@
 N = 10;
 traj_list = cell(N,2);
 
-for idx=0:N
+for idx=0:N-1
     x0 = [idx;9;0;0];
     [utraj,xtraj] = getTrajectory(x0);
-    traj_list{idx,1} = xtraj;
-    traj_list{idx,2} = utraj;
+    traj_list{idx+1,1} = xtraj;
+    traj_list{idx+1,2} = utraj;
 end
 
 
