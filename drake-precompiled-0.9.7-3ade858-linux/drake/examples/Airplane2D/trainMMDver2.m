23d00a79743cb2f9c893d6adff16bdd00bdfd577
Beomjoon Kim <beomjoon.kim0@gmail.com>
2015-01-19 09:51:32 -0500

    dafjsadf

--- drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainMMDver2.m
+++ drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainMMDver2.m
@@ -6,7 +6,7 @@ function [controller,mmd_data] = trainMMDver2(x0,n_mmd_itern,alpha_list)
     ref_traj_list = cell(numel(alpha_list,2),1);
     tf_list = zeros(numel(alpha_list,2),1);
     
-    for idx=1:numel(alpha_list)
+    for idx=1:numel(alpha_list)-1
         alpha = alpha_list(idx);
         init_fname = sprintf('initial_mmd_traj_alpha=%d.mat',alpha);
         if ~exist(init_fname,'file')
@@ -26,12 +26,12 @@ function [controller,mmd_data] = trainMMDver2(x0,n_mmd_itern,alpha_list)
         controller = setNewController(controller,x,y);
         
         % get reference trajectory
-        ref_traj_list{idx,1} = controller.data_sets_unnormalized{idx,1}(1:4,:); 
+        ref_traj_list{idx,1} = x(1:4,:); 
     end
     
     
     % set parameters
-    beta = 0.94;
+    beta = 1.1;
     for MMD_iteration = 1:n_mmd_itern
             x = []; y=[];     
             
@@ -39,7 +39,7 @@ function [controller,mmd_data] = trainMMDver2(x0,n_mmd_itern,alpha_list)
                 d_list =[];
                 alpha = alpha_list(alpha_idx);
                 
-                tf = tf_list(alpha_idx);
+                tf = tf_list(1);
                 N  = numel(0:dt:tf);
                 x1=zeros(4,N); x1(:,1) = x0; % state simulation
                 p = PlanePlant(alpha);
@@ -47,9 +47,7 @@ function [controller,mmd_data] = trainMMDver2(x0,n_mmd_itern,alpha_list)
                 for k=1:N-1
                     current_state = [x1(:,k);alpha];
                     [d,min_idx] = checkDiscrepancy(controller,current_state); 
-                    if d > 0.99
-                        d,current_state
-                    end
+                    d,current_state,min_idx
                     d_list=[d_list d];
                     
                     dist_to_goal = norm(x1(1:2,k)-[5; 9]);
@@ -59,14 +57,13 @@ function [controller,mmd_data] = trainMMDver2(x0,n_mmd_itern,alpha_list)
 
                     % Check if the encountered state lies far from datasets
                     if d > beta
-                        d,x1(:,k)
                         % get the reference trajectory for this alpha value
-                        ref_traj = ref_traj_list{alpha_idx,1};
-                        [u_traj_from_curr_loc,x_traj_from_curr_loc,F] = getRecoveryTrajectory(x1(:,k),alpha,false,ref_traj);
+                        %ref_traj = ref_traj_list{alpha_idx,1};
+                        %[u_traj_from_curr_loc,x_traj_from_curr_loc,F] = getRecoveryTrajectory(x1(:,k),alpha,false,ref_traj);
+
+                        [u_traj_from_curr_loc,x_traj_from_curr_loc,F] = getTrajectory(x1(:,k),alpha,false);
                         %save(sprintf('traj%d',controller.n_mmd_itern),'x_traj_from_curr_loc','u_traj_from_curr_loc','F');
-                        action_diff = u_traj_from_curr_loc.eval(0) - controller.predict(current_state,min_idx); 
-                        action_diff
-                        
+
                         temp_tf = x_traj_from_curr_loc.getBreaks; temp_tf = temp_tf(end);
                         t=0:0.01:temp_tf;
                         [x_to_attach,y_to_attach] = turnTrajToData(x_traj_from_curr_loc,u_traj_from_curr_loc,t,alpha);
@@ -77,8 +74,12 @@ function [controller,mmd_data] = trainMMDver2(x0,n_mmd_itern,alpha_list)
 
                         x = [x x_to_attach]; 
                         y = [y y_to_attach];
+                        action_diff = abs(u_traj_from_curr_loc.eval(0) - controller.predict(current_state))
                         controller = setNewController(controller,x_to_attach,y_to_attach);
-                        control = controller.predict(current_state);
+                        %control = controller.predict(current_state);
+                        control = u_traj_from_curr_loc.eval(0);
+                        action_diff = abs(u_traj_from_curr_loc.eval(0) - controller.predict(current_state)) 
+                        
                     else
                         control = controller.predict(current_state);
                     end
--- drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainMMDwithAlphaver2.m
+++ drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainMMDwithAlphaver2.m
@@ -4,4 +4,4 @@ train_alpha_list = [5,7];
 n_mmd_itern = 5;
 x0 = [3.9;0;0;0];
 [controller,data] = trainMMDver2(x0,n_mmd_itern,train_alpha_list);
-save('vary_alpha_mmd_results_only_alpha=5_ver6','controller','data','train_alpha_list','train_time');
+save('vary_alpha_mmd_results_only_alpha=5_ver6','controller','data','train_alpha_list');
