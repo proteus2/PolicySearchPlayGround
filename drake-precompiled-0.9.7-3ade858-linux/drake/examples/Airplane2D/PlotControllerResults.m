591839f5173c4e1c78a8ea6d55c68f1be026121f
beomjoon <beomjoon.kim0@gmail.com>
2015-01-22 17:16:56 -0500

    asdlkfjasjf

--- drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/PlotControllerResults.m
+++ drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/PlotControllerResults.m
@@ -1,92 +1,15 @@
-% Compare controllers
-x0 = [3.9;0;0;0];
-rand_list = rand(1,50);
-alpha_list = rand_list*30 + (1-rand_list)*4;
+fname = 'cost_list_all_alpha_test_a=[4,30]';
+load(fname);
 
+temp1 = alpha_list;
+temp2 = cost_list_all_alpha;
 
-load('cost_list_all_alpha','alpha_list')
-cost_list_all_alpha = {};
-alpha_idx = 1;
-for alpha = alpha_list
-    optimaltraj_fname = sprintf('optimal_traj_with_alpha=%d,x0=[%0.2f,%0.2f,%0.2f,%0.2f].mat',alpha,x0(1),x0(2),x0(3),x0(4));
-    optimaltraj_fname = strcat('./data_for_plots/test/',optimaltraj_fname);
-    if exist(optimaltraj_fname,'file')
-        getOptimalTraj = false;
-    else
-        getOptimalTraj = true;
-    end
+fname2 = 'cost_list_all_alpha_a=[3,10]';
+load(fname2);
 
-    if getOptimalTraj 
-        [optimal_u,optimal_x] = getTrajectory(x0,alpha,false);         
-        u{1,1} = optimal_u;
-        tf=optimal_u.getBreaks; tf=tf(end);
-        [traj_list_opt,traj_opt_cost]=EvaluateControllers(u,x0,tf,alpha);
-        save(optimaltraj_fname,'optimal_u','optimal_x','alpha','traj_list_opt','traj_opt_cost');
-    else
-        load(optimaltraj_fname);
-        u{1,1} = optimal_u;
-        tf=optimal_u.getBreaks; tf=tf(end);
-        [traj_list_opt,traj_opt_cost]=EvaluateControllers(u,x0,tf,alpha);
-        save(optimaltraj_fname,'optimal_u','optimal_x','alpha','traj_list_opt','traj_opt_cost');
-    end
-    
+alpha_list = [alpha_list temp1];
+cost_list_all_alpha = [cost_list_all_alpha;temp2];
 
-    apprxtraj_fname = sprintf('appx_traj_with_alpha=%d,x0=[%0.2f,%0.2f,%0.2f,%0.2f].mat',alpha,x0(1),x0(2),x0(3),x0(4));
-    
-    train_file1 = 'mmd_results_repmat=1,a=all';
-    train_file2 = 'vary_alpha_supervised_results_alpha=all,iter=5';
-    apprxtraj_fname = strcat('./data_for_plots/test/',train_file1,'_',train_file2,'_',apprxtraj_fname);
-    
-    if ~exist(apprxtraj_fname,'file')
-        tf = optimal_u.getBreaks; tf=tf(end);
-        load(train_file1);
-        ctrl_list{1,1} = controller;
-        
-        load(train_file2);
-        ctrl_list{2,1} = controller;
-
-        [traj_list,cost_list]=EvaluateControllers(ctrl_list,x0,tf,alpha);
-        save(apprxtraj_fname,'traj_list','cost_list','alpha','ctrl_list')
-    else
-        load(apprxtraj_fname)
-        
-        [traj_list,cost_list]=EvaluateControllers(ctrl_list,x0,tf,alpha);
-        save(apprxtraj_fname,'traj_list','cost_list','alpha')
-    end
-    cost_list_all_alpha{alpha_idx,1} = traj_opt_cost;
-    cost_list_all_alpha{alpha_idx,2} = cost_list(1,:);
-    cost_list_all_alpha{alpha_idx,3} = cost_list(2,:);
-    
-    traj_list_all_alpha{alpha_idx,1} = traj_list{1};
-    traj_list_all_alpha{alpha_idx,2} = traj_list{2};
-    alpha_idx = alpha_idx+1;
-%     
-%      x1 = traj_list{1,1}.eval(traj_list{1,1}.getBreaks);
-%      x2 = traj_list{2,1}.eval(traj_list{1,1}.getBreaks);
-%      optx = optimal_x.eval(optimal_x.getBreaks);
-%      
-%      figure; scatter(x1(1,:),x1(2,:),'r');
-%      hold on; scatter(x2(1,:),x2(2,:),'blue');
-%      hold on; scatter(optx(1,:),optx(2,:),'black');
-%      legend('mmd','sup','opt','Location','southwest');
-% 
-%      figure;bar(-[cost_list(1,1),cost_list(2,1),traj_opt_cost(1,1)])
-%      ax = gca;
-%      ax.XTickLabel = {'mmd','supervised','trajopt'};
-%      ylabel('Accumulated Rewards')
-%      xlabel('Algorithms')
-%      
-%      figure;bar(-[cost_list(1,2),cost_list(2,2),traj_opt_cost(1,2)])
-%      ax = gca;
-%      ax.XTickLabel = {'mmd','supervised','trajopt'};
-%      ylabel('Average Rewards')
-%      xlabel('Algorithms')
-%      
-%      visualizeTraj(traj_list{1,1},alpha);
-%      visualizeTraj(traj_list{2,1},alpha);
-end
-%save('cost_list_all_alpha_ver2','cost_list_all_alpha','alpha_list');
-% 
 traj_opt_cost = zeros(size(cost_list_all_alpha,1),2);
 
 ctrl1_cost = zeros(size(cost_list_all_alpha,1),2);
@@ -102,6 +25,9 @@ for idx = 1:size(cost_list_all_alpha,1)
     ctrl2_cost(idx,1) = cost_list_all_alpha{idx,3}(1);
     ctrl2_cost(idx,2) = cost_list_all_alpha{idx,3}(2);
 end
+traj_opt_cost = (traj_opt_cost);
+ctrl1_cost = (ctrl1_cost);
+ctrl2_cost = (ctrl2_cost);
 
 topt_mean = mean(traj_opt_cost);
 topt_std = std(traj_opt_cost);
@@ -111,5 +37,18 @@ c1_std = std(ctrl1_cost);
 c2_mean = mean(ctrl2_cost);
 c2_std = std(ctrl2_cost);
 
-abs(c1_mean-topt_mean)
-abs(c1_mean-c2_mean)
+% 95% confidence interval
+n=numel(alpha_list);
+c1_CI = 1.96*c1_std/sqrt(n);
+c2_CI = 1.96*c2_std/sqrt(n);
+topt_CI = 1.96*topt_std/sqrt(n);
+
+figure;bar(-[c1_mean(1,1),c2_mean(1,1),topt_mean(1,1)]) 
+hold on; errorbar([-c1_mean(1,1),-c2_mean(1,1),-topt_mean(1,1)],[c1_CI(1),c2_CI(1),topt_CI(1)],'o');
+
+ ax = gca;
+ ax.XTickLabel = {'mmd','supervised','trajopt'};
+ ylabel('Accumulated Rewards')
+ xlabel('Algorithms')
+     
+     
--- drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/evaluateTrajCost.m
+++ drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/evaluateTrajCost.m
@@ -1,13 +1,13 @@
-%TODO: Test this function
+% TODO: 
 function [tot_cost,avg_cost_per_step] = evaluateTrajCost(xtraj,utraj,field,t)
    
    tot_cost = 0;
    for step = 1:size(t(1:end-1),2)
-       x = xtraj.eval(step); u = utraj.eval(step);
+       x = xtraj.eval(t(step)); u = utraj.eval(t(step));
        tot_cost = tot_cost + cost(0,x,u,field);
    end 
     
-   tot_cost = tot_cost + finalCost(t(end),xtraj.eval(t(end)));
+   tot_cost = tot_cost + finalCost(t(end),xtraj.eval(t(end)))
    avg_cost_per_step = tot_cost/size(t,2);
 end
 
@@ -22,7 +22,7 @@ R = 0.0001;
 dc = dc(i,:);
 
 xg=[5;9;0;0];
-g = (10*c + u'*R*u);
+g = (c + u'*R*u);
 %g = c + norm(xg-x);
 %g = sum((R*u).*u,1);
 dg = [0,dc,2*u'*R];
@@ -31,7 +31,7 @@ dg = [0,dc,2*u'*R];
 end
 
 function [h,dh] = finalCost(t,x)
-xg=[5;9;0;0];
-h = 100*(norm(xg(1:2,:)-x(1:2,:)));
-dh = [1,zeros(1,size(x,1))];
+    xg=[5;9;0;0];
+    h = 10*(norm(xg(1:2,:)-x(1:2,:)));
+    dh = [1,zeros(1,size(x,1))];
 end
