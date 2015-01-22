c0864aeebce8c1d3b811f0ef6a3909de421451c6
beomjoon <beomjoon.kim0@gmail.com>
2015-01-22 12:01:28 -0500

    aklsdfjlksadf

--- drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainMMDwithAlpha_temp.m
+++ drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainMMDwithAlpha_temp.m
@@ -3,4 +3,4 @@ train_alpha_list = [5,7,12,14,15,18,20,22,24];
 n_mmd_itern = 50;
 x0 = [3.9;0;0;0];
 [controller,data] = trainMMD(x0,n_mmd_itern,train_alpha_list);
-save('mmd_results_repmat=10,a=all','controller','data','train_alpha_list');
+save('mmd_results_repmat=1,a=all','controller','data','train_alpha_list');
