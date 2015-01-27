3400ce6ea2d2e17254344a3a658a73b50aad1d93
Beomjoon Kim <beomjoon.kim0@gmail.com>
2015-01-27 16:02:10 -0500

    aldf

--- drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainMMDwithAlpha.m
+++ drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainMMDwithAlpha.m
@@ -32,9 +32,9 @@ aggregate=true
 [controller,data] = trainMMD(train_x0_list,n_mmd_itern,train_alpha_list,aggregate);
 
 if aggregate
-    save('mmd_results_init_at_alpha_R>0_agg','controller','data','train_alpha_list','train_x0_list');
+    save('mmd_results_alpha_R>0_agg','controller','data','train_alpha_list','train_x0_list');
 else
-    save('mmd_results_init_at_alpha_R>0','controller','data','train_alpha_list','train_x0_list');
+    save('mmd_results_alpha_R>0','controller','data','train_alpha_list','train_x0_list');
 end
 % save('mmd_results_repmat=1,a=all,algo=RF,aggregation','controller','data','train_alpha_list');
 % load('mmd_results_x0=2,4')
