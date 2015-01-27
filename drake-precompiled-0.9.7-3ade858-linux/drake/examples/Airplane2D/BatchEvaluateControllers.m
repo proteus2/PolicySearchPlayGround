5cbee2b3623e8bddd8709bd8da35c5a9d2c581e8
Beomjoon Kim <beomjoon.kim0@gmail.com>
2015-01-27 18:44:19 -0500

    af

--- drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/BatchEvaluateControllers.m
+++ drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/BatchEvaluateControllers.m
@@ -1,7 +1,7 @@
 % Compare controllers
 clear all;
 close all;
-x0 = [4;0;0;0];
+x0 = [2;0.7;0;0];
 rand_list = rand(1,100);
 alpha_list = rand_list*30 + (1-rand_list)*4;
 
@@ -11,111 +11,125 @@ load('cost_list_all_alpha_a=[3,10]_repmat=1','alpha_list');
 alpha_list = [alpha_list temp];
 cost_list_all_alpha = {};
 
-alpha_list = alpha_list(81);
 
-alpha_list =10;
-alpha_idx =    1;
-for alpha = alpha_list
-    optimaltraj_fname = sprintf('optimal_traj_with_alpha=%d,x0=[%0.2f,%0.2f,%0.2f,%0.2f].mat',alpha,x0(1),x0(2),x0(3),x0(4));
-    optimaltraj_fname = strcat('./data_for_plots/test/',optimaltraj_fname);
-    if exist(optimaltraj_fname,'file')
-        getOptimalTraj = false;
-    else
-        getOptimalTraj = true;
-    end
+n_x0=10;
+x0_rand = rand(1,n_x0);
+x0_x = 5*x0_rand + (1-x0_rand)*2;
+x0_rand = rand(1,n_x0);
+x0_y = 0*x0_rand + (1-x0_rand)*1;
 
-    if getOptimalTraj 
-        [optimal_u,optimal_x] = getTrajectory(x0,alpha,false);         
-        u{1,1} = optimal_u;
-        tf=optimal_u.getBreaks; tf=tf(end);
-        [traj_list_opt,traj_opt_cost]=EvaluateControllers(u,x0,tf,alpha);
-        save(optimaltraj_fname,'optimal_u','optimal_x','alpha','traj_list_opt','traj_opt_cost');
-    else
-        load(optimaltraj_fname);
-        u{1,1} = optimal_u;
-        tf=optimal_u.getBreaks; tf=tf(end);
-        [traj_list_opt,traj_opt_cost]=EvaluateControllers(u,x0,tf,alpha);
-        save(optimaltraj_fname,'optimal_u','optimal_x','alpha','traj_list_opt','traj_opt_cost');
-    end
-    
+if ~exist('./test_alpha_list.mat','file')
+    test_alpha_list = 5*x0_rand + (1-x0_rand)*20;
+    save('test_alpha_list','test_alpha_list');
+else
+    load('test_alpha_list')
+end
 
-    apprxtraj_fname = sprintf('appx_traj_with_alpha=%d,x0=[%0.2f,%0.2f,%0.2f,%0.2f].mat',alpha,x0(1),x0(2),x0(3),x0(4));
-    
+if ~exist('./test_x0_list.mat','file')
+    test_x0_list = [x0_x;x0_y;zeros(1,n_x0);zeros(1,n_x0)];
+    test_x0_list = [test_x0_list [4;0;0;0]];
+    save('test_x0_list','test_x0_list');
+else
+    load('test_x0_list')
+end
 
-    train_file1 = 'mmd_results_repmat=1,a=all,algo=nearest_neighbor,aggregation';
-    train_file2 = 'mmd_results_repmat=1,a=all_nearest_neighbor';
-    train_file3 = 'mmd_results_repmat=1,a=all,algo=RF,aggregation';
-    train_file4 = 'mmd_results_repmat=10,a=all';
-    
-    train_files={'mmd_results_repmat=1,a=all,algo=nearest_neighbor,aggregation','mmd_results_repmat=1,a=all_nearest_neighbor',...
-                     'mmd_results_repmat=1,a=all,algo=RF,aggregation','mmd_results_repmat=10,a=all'};
-    train_files={'mmd_results_init_at_alpha_R>0_agg','mmd_results_alpha_R>0'};
-    n_files = size(train_files,2);
-    
-    %apprxtraj_fname = strcat('./data_for_plots/test/',train_file1,'_',train_file2,'_',train_file3,'_',apprxtraj_fname);
-    apprxtraj_fname = './data_for_plots/test/';
-    for idx=1:n_files
-        fname = train_files{1,idx};
-        if idx == n_files
-            apprxtraj_fname = strcat(apprxtraj_fname,fname);
+alpha_list = test_alpha_list;
+
+%% Eval script
+    alpha_idx =    1;
+for x0_idx=1:size(test_x0_list,2)
+    x0 = test_x0_list(:,x0_idx);
+
+    for alpha = alpha_list
+        optimaltraj_fname = sprintf('optimal_traj_with_alpha=%d,x0=[%0.2f,%0.2f,%0.2f,%0.2f].mat',alpha,x0(1),x0(2),x0(3),x0(4));
+        optimaltraj_fname = strcat('./data_for_plots/test/',optimaltraj_fname);
+        if exist(optimaltraj_fname,'file')
+            getOptimalTraj = false;
         else
-            apprxtraj_fname = strcat(apprxtraj_fname,fname,'_');
+            getOptimalTraj = true;
         end
-    end
-    
-    if ~exist(apprxtraj_fname,'file')
-        tf = optimal_u.getBreaks; tf=tf(end);
-        ctrl_list = cell(n_files,1);
+
+        if getOptimalTraj 
+            [optimal_u,optimal_x] = getTrajectory(x0,alpha,false);         
+            u{1,1} = optimal_u;
+            tf=optimal_u.getBreaks; tf=tf(end);
+            [traj_list_opt,traj_opt_cost]=EvaluateControllers(u,x0,tf,alpha);
+            save(optimaltraj_fname,'optimal_u','optimal_x','alpha','traj_list_opt','traj_opt_cost');
+        else
+            load(optimaltraj_fname);
+            u{1,1} = optimal_u;
+            tf=optimal_u.getBreaks; tf=tf(end);
+            [traj_list_opt,traj_opt_cost]=EvaluateControllers(u,x0,tf,alpha);
+            save(optimaltraj_fname,'optimal_u','optimal_x','alpha','traj_list_opt','traj_opt_cost');
+        end
+
+        train_files={'mmd_results_init_at_alpha_R>0_agg'};
+        n_files = size(train_files,2);
+
+        apprxtraj_fname = './data_for_plots/test/';
+        for idx=1:n_files
+            fname = train_files{1,idx};
+            if idx == n_files
+                apprxtraj_fname = strcat(apprxtraj_fname,fname);
+            else
+                apprxtraj_fname = strcat(apprxtraj_fname,fname,'_');
+            end
+        end
+
+        if ~exist(apprxtraj_fname,'file')
+            tf = optimal_u.getBreaks; tf=tf(end);
+            ctrl_list = cell(n_files,1);
+            for fidx = 1:n_files
+                train_file = train_files{1,fidx};
+                load(train_file);
+                ctrl_list{fidx,1} = controller;
+            end
+            [traj_list,cost_list]=EvaluateControllers(ctrl_list,x0,tf,alpha);
+            save(apprxtraj_fname,'traj_list','cost_list','alpha','ctrl_list')
+        else
+            load(apprxtraj_fname)
+            tf = optimal_u.getBreaks; tf=tf(end)+0.01;
+            [traj_list,cost_list]=EvaluateControllers(ctrl_list,x0,tf,alpha);
+            save(apprxtraj_fname,'traj_list','cost_list','alpha','ctrl_list')
+        end
+
+        cost_list_all_alpha{alpha_idx,1} = traj_opt_cost;
         for fidx = 1:n_files
-            train_file = train_files{1,fidx};
-            load(train_file);
-            ctrl_list{fidx,1} = controller;
+            cost_list_all_alpha{alpha_idx,fidx+1} = cost_list(fidx,:);
+            traj_list_all_alpha{alpha_idx,fidx} = traj_list{fidx};
         end
-        [traj_list,cost_list]=EvaluateControllers(ctrl_list,x0,tf,alpha);
-        save(apprxtraj_fname,'traj_list','cost_list','alpha','ctrl_list')
-    else
-        load(apprxtraj_fname)
-        tf = optimal_u.getBreaks; tf=tf(end)+0.01;
-        [traj_list,cost_list]=EvaluateControllers(ctrl_list,x0,tf,alpha);
-        save(apprxtraj_fname,'traj_list','cost_list','alpha','ctrl_list')
+
+        alpha_idx = alpha_idx+1;
+    %     
+    %      x1 = traj_list{1,1}.eval(traj_list{1,1}.getBreaks);
+    %      x2 = traj_list{2,1}.eval(traj_list{1,1}.getBreaks);
+    %      optx = optimal_x.eval(optimal_x.getBreaks);
+    %      
+    %      figure; scatter(x1(1,:),x1(2,:),'r');
+    %      hold on; scatter(x2(1,:),x2(2,:),'blue');
+    %      hold on; scatter(optx(1,:),optx(2,:),'black');
+    %      legend('mmd','sup','opt','Location','southwest');
+    % 
+    %      figure;bar(-[cost_list(1,1),cost_list(2,1),traj_opt_cost(1,1)])
+    %      ax = gca;
+    %      ax.XTickLabel = {'mmd','supervised','trajopt'};
+    %      ylabel('Accumulated Rewards')
+    %      xlabel('Algorithms')
+    %      
+    %      figure;bar(-[cost_list(1,2),cost_list(2,2),traj_opt_cost(1,2)])
+    %      ax = gca;
+    %      ax.XTickLabel = {'mmd','supervised','trajopt'};
+    %      ylabel('Average Rewards')
+    %      xlabel('Algorithms')
+           %visualizeTraj(optimal_x,alpha);
+           %visualizeTraj(traj_list{1,1},alpha);
+          %visualizeTraj(traj_list{2,1},alpha);
+    %      visualizeTraj(traj_list{3,1},alpha);
+
+    %       visualizeTraj(traj_list{4,1},alpha);
+
+
     end
-    
-    cost_list_all_alpha{alpha_idx,1} = traj_opt_cost;
-    for fidx = 1:n_files
-        cost_list_all_alpha{alpha_idx,fidx+1} = cost_list(fidx,:);
-        traj_list_all_alpha{alpha_idx,fidx} = traj_list{fidx};
-    end
-    
-    alpha_idx = alpha_idx+1;
-%     
-%      x1 = traj_list{1,1}.eval(traj_list{1,1}.getBreaks);
-%      x2 = traj_list{2,1}.eval(traj_list{1,1}.getBreaks);
-%      optx = optimal_x.eval(optimal_x.getBreaks);
-%      
-%      figure; scatter(x1(1,:),x1(2,:),'r');
-%      hold on; scatter(x2(1,:),x2(2,:),'blue');
-%      hold on; scatter(optx(1,:),optx(2,:),'black');
-%      legend('mmd','sup','opt','Location','southwest');
-% 
-%      figure;bar(-[cost_list(1,1),cost_list(2,1),traj_opt_cost(1,1)])
-%      ax = gca;
-%      ax.XTickLabel = {'mmd','supervised','trajopt'};
-%      ylabel('Accumulated Rewards')
-%      xlabel('Algorithms')
-%      
-%      figure;bar(-[cost_list(1,2),cost_list(2,2),traj_opt_cost(1,2)])
-%      ax = gca;
-%      ax.XTickLabel = {'mmd','supervised','trajopt'};
-%      ylabel('Average Rewards')
-%      xlabel('Algorithms')
-       %visualizeTraj(optimal_x,alpha);
-       %visualizeTraj(traj_list{1,1},alpha);
-      %visualizeTraj(traj_list{2,1},alpha);
-%      visualizeTraj(traj_list{3,1},alpha);
-
-%       visualizeTraj(traj_list{4,1},alpha);
-
-
 end
 keyboard;
 save('cost_list_all_alpha_a=[3,30]_all_algo_if_cost_fun__','cost_list_all_alpha','alpha_list','traj_list_all_alpha');
