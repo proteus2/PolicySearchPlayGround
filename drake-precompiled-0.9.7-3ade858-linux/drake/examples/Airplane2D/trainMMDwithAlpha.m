1a1c41d10aa48b9b52db0805332b5d0de184e353
Beomjoon Kim <beomjoon.kim0@gmail.com>
2015-02-15 11:42:30 -0500

    sldjfa

--- drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainMMDwithAlpha.m
+++ drake-precompiled-0.9.7-3ade858-linux/drake/examples/Airplane2D/trainMMDwithAlpha.m
@@ -4,12 +4,12 @@ train_x0_list = {[2;0;0;0],[4;0;0;0]};
 
 n_x0=30;
 x0_rand = rand(1,n_x0);
-x0_x = 2*x0_rand + (1-x0_rand)*8;
+x0_x = 2*x0_rand + (1-x0_rand)*4;
 x0_rand = rand(1,n_x0);
-x0_y = 0*x0_rand + (1-x0_rand)*0;
+x0_y = 0*x0_rand + (1-x0_rand)*1;
 
 if ~exist('./train_alpha_list.mat','file')
-    train_alpha_list = 5*x0_rand + (1-x0_rand)*20;
+    train_alpha_list = 5*x0_rand + (1-x0_rand)*30;
     save('train_alpha_list','train_alpha_list');
 else
     load('train_alpha_list')
